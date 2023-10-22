d6d53844f8428f9485eb36a4d83e2f85ed21ece2
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-10-22 11:26:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-//your JS code here. If required.
+const output = document.getElementById("output");
+
+function getArray(arr) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(arr)
+        }, 3000)
+    })
+}
+
+function getOddArray(arr) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let oddArr = arr.filter((value) => value % 2 !== 0);
+            resolve(oddArr)
+        }, 1000)
+    })
+}
+
+function getEvenArray() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let evenArr = arr.map((value) => value * 2);
+            resolve(evenArr)
+        }, 2000)
+    })
+}
+
+let arr = [1, 2, 3, 4];
+async function getResults(arr) {
+    const response = await Promise.all([getArray(arr), getOddArray(arr), getEvenArray(arr)])
+    const res = await response;
+    result = res
+    return result
+}
+    
+let result = getResults(arr)
+setTimeout(() => {
+    output.innerText = result[0].join(" ")
+    setTimeout(() => {
+        output.innerText = result[1].join(" ")
+        setTimeout(() => {
+            output.innerText = result[2].join(" ")
+        }, 1000);
+    }, 2000);
+}, 3000);
